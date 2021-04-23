a16327f209e1f5d44e4a7e933d240db7435f58c6
Marcos Paes - Prime <marcoss.prime@superdigital.com.br>
2021-04-23 15:22:30 -0300

    include itens

---
+++ DotNet5_Refit_API_Test/DotNet5_Refit_API_Test/API_CreateUser/ApiClients/IPersonClient.cs
@@ -0,0 +1,50 @@
+﻿using Refit;
+
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Super.EWalletCore.PersonDataManagement.Application.IntegratedTests.ApiClient
+{
+    public interface IPersonClient
+    {
+        [Post("/run-create-person")]
+        public Task<HttpResponseMessage> RunCreatePerson([Body] CreatePersonRequest request, [Header("x-country")] int country);
+    }
+
+    public class CreatePersonResponse_Success
+    {
+        public string PersonId { get; set; }
+    }
+
+    public class CreatePersonResponse_Failure
+    {
+        public string Code { get; set; }
+        public string Id { get; set; }
+        public string Message { get; set; }
+        public List<Error> Errors { get; set; }
+    }
+
+    public class Error
+    {
+        public string ErrorCode { get; set; }
+        public string Message { get; set; }
+        public string Path { get; set; }
+    }
+
+    public class BadRequest
+    {
+        public string Code { get; set; }
+        public Guid Id { get; set; }
+        public string Message { get; set; }
+        public string Errors { get; set; }
+    }
+
+    public class Errors
+    {
+        public string ErrorCode { get; set; }
+        public string Message { get; set; }
+    }
+}
+
---
+++ DotNet5_Refit_API_Test/DotNet5_Refit_API_Test/API_CreateUser/Setup/Startup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using Super.EWalletCore.PersonDataManagement.Application.IntegratedTests.ApiClient;
+
+using System;
+
+namespace Super.EWalletCore.PersonDataManagement.Application.IntegratedTests.Setup
+{
+    public class IntegratedTestsStartup : TestHost
+    {
+        public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSingleton<PersonService>();
+
+            var settings = configuration.GetSection(nameof(TestSettings)).Get<TestSettings>();
+
+            var embossingUrl = settings.SelectedEnvironment switch
+            {
+                EEnvironment.Dev => settings.Environments.Dev.EmbossingUrl,
+                EEnvironment.Qa => settings.Environments.Qa.EmbossingUrl,
+                _ => throw new NotImplementedException()
+            };
+
+            services.AddRefitClient<IPersonClient>()
+                .ConfigureHttpClient(c => c.BaseAddress = new Uri(embossingUrl));
+
+        }
+    }
+}
+
